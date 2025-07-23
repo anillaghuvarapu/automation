20ade9f00eb4e3560047f1c22798be2ae4002f98
anillaghuvarapu <mailtokumar555@gmail.com>
2025-07-23 21:58:28 +0530

    new changes

--- aws/class2/05-list-variable/variable.tf
+++ aws/class2/05-list-variable/variable.tf
@@ -1,5 +1,5 @@
 variable "user_names" {
   description = "IAM usernames"
   type        = list(string)
-  default     = ["user1", "user2", "user3"]
+  default     = ["user1", "user2", "user3", "user4", "devuser"]
 }
