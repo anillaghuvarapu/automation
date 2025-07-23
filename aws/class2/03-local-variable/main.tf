0db8778840350a3d4b4c4f29b4211baf199ed99d
anillaghuvarapu <mailtokumar555@gmail.com>
2025-07-23 21:45:08 +0530

    new changes

--- aws/class2/03-local-variable/main.tf
+++ aws/class2/03-local-variable/main.tf
@@ -1,13 +1,15 @@
 variable "bucket_prefix" {
   type    = string
-  default = "careerbytecode-all-in-one-tech-job-platform"
+  default = "primitive-s3"
 }
 
 locals {
-  bucket_name = "${var.bucket_prefix}-bucket1"
+  bucket_name = "${var.bucket_prefix}-bucket-new"
 }
 
-resource "aws_s3_bucket" "my_test_bucket" {
+resource "aws_s3_bucket" "my_bucket_usecases" {
   bucket = local.bucket_name
-  acl    = "public-read-write"
+
 }
+
+# acl to be checked and updated as per requirements
