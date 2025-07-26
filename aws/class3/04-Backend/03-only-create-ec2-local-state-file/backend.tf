601abe907974d9c062ee626bbc2dfdc07ecfc319
anillaghuvarapu <mailtokumar555@gmail.com>
2025-07-26 17:48:06 +0530

    mentioning the dynamodb in backend s3 deprecated and we have to use use_lockfile

--- aws/class3/04-Backend/03-only-create-ec2-local-state-file/backend.tf
+++ aws/class3/04-Backend/03-only-create-ec2-local-state-file/backend.tf
@@ -1,12 +1,9 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket = "cbc-platform-bucket1"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-1"
-
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
+    bucket       = "platform-bucket3402"
+    key          = "global/s3/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
--- aws/class3/04-Backend/03-only-create-ec2-local-state-file/ec2.tf
+++ aws/class3/04-Backend/03-only-create-ec2-local-state-file/ec2.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "example1" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+}
