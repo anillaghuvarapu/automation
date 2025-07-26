f3e8886f488b43040703dbcda7ad85d7e9b6dd7a
anillaghuvarapu <mailtokumar555@gmail.com>
2025-07-26 17:56:30 +0530

    versioning enable, we have to empty the bucket before destroy

--- aws/class3/04-Backend/01-firstbucket-creation/s3bucket.tf
+++ aws/class3/04-Backend/01-firstbucket-creation/s3bucket.tf
@@ -1,7 +1,14 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "cbc-platform-bucket1"
+  bucket = "platform-bucket3402"
 }
 
+resource "aws_s3_bucket_versioning" "name" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.example.id
   rule {
