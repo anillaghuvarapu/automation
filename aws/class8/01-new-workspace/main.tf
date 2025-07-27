a80096509d12267b1b6bd26b5e3b72ee130f49a1
anillaghuvarapu <mailtokumar555@gmail.com>
2025-07-27 20:15:30 +0530

    updates in auto.tfvars file

--- aws/class8/01-new-workspace/main.tf
+++ aws/class8/01-new-workspace/main.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "workspace_ec2" {
+resource "aws_instance" "ec2_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   key_name               = var.key_name
   vpc_security_group_ids = var.vpc_security_group_ids
+  #workspace_name         = var.workspace_name
 
   tags = {
     Name      = "${terraform.workspace}-EC2"
--- aws/class8/01-new-workspace/qa.auto.tfvars
+++ aws/class8/01-new-workspace/qa.auto.tfvars
@@ -2,5 +2,6 @@ aws_region             = "us-east-1"
 instance_type          = "t2.micro"
 ami_id                 = "ami-09e6f87a47903347c"
 key_name               = "cbcqapoc"
-vpc_security_group_ids = ["sg-0552081e06fd3efce"]
-subnet_id              = "subnet-0c20ac9870534dfc2"
+vpc_security_group_ids = ["sg-03d3c898ae01a2852"]   # Replace with your security group ID
+subnet_id              = "subnet-0c5c37f1abb88b9e6" # Replace with your subnet ID
+
